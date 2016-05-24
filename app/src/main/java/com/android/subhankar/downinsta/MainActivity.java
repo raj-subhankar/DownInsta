5b334f1542fd9b9f7c557188b20fe4844b6a800f
Subhankar Raj <subhankar.rj@gmail.com>
2016-05-24 10:20:38 +0530

    Hide soft keyboard

--- app/src/main/java/com/android/subhankar/downinsta/MainActivity.java
+++ app/src/main/java/com/android/subhankar/downinsta/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -45,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         editTextUrl = (EditText) findViewById(R.id.editText);
-
+        editTextUrl.setInputType(InputType.TYPE_NULL);
         imgDisplay = (ImageView) findViewById(R.id.imageView);
         tvPercent = (TextView) findViewById(R.id.tvProgress);
         pbLoading = (ProgressBar) findViewById(R.id.progressBar);
